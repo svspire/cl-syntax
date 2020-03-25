cd1b84c11544283f15e7917a2c4fd9e1913dd1fc
Shannon Spires <ssos@bearlanding.com>
2020-03-25 17:06:57 -0600

    Special version that calls (break) when you try to load it. Why? Through long and painful experience, I've found that the use of @doc strings in Common Lisp source causes all sorts of problems with my IDE in CCL. Furthermore, such syntax is fundamentally NOT LISP. It's Python, and Python syntax doesn't belong in Common Lisp. And furthermore, Common Lisp itself provides quite a nice syntax for documenting its definitions, and there's absolutely no reason to use @doc rather than the built-in mechanism.
    Finally, cl-syntax and cl-annot require special reader macros that
    mess with the readtable, and that causes no end of problems with
    CCL. If that were actually necessary, I'd try to make it work, but
    in my experience 90% of the reason people use cl-syntax and cl-annot
    is to use Python-style docstrings, which in no case should be
    considered necessary.

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,5 +1,15 @@
 (in-package :cl-user)
 
+(eval-when (:load-toplevel :compile-toplevel :execute)
+  (break "You are attempting to load cl-syntax. I (Shannon Spires) strongly dislike
+this software both on philosophical grounds (because it's most often used
+merely as a substrate to allow cl-annot, which injects Python
+syntax into Common Lisp source code) and because it requires custom
+reader-macros which play havoc with most implementations of Common Lisp.
+I suggest you modify whatever systems you're using that require this
+library such that they no longer require it."))
+
+
 (defpackage :cl-syntax
   (:nicknames :syntax)
   (:use :cl :named-readtables)
